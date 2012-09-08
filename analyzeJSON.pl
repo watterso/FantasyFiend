50a99cc001a4ac311b84096f09928b96a7b6246a
James Watterson <jameslwatterson@gmail.com>
2012-09-08 01:13:36 -0400

    Revamp: all in one script now
    deleted some scripts, kept the *JSON.pl ones for backup.
    Interaction is now in a pseudo shell try the 'comp' command!

--- analyzeJSON.pl
+++ analyzeJSON.pl
@@ -5,18 +5,49 @@ use List::Util qw(sum);
 use JSON;
 use Switch;
 use strict;
+use Math::Complex;
+use Math::NumberCruncher;
+use IO::Scalar;
 
 sub addValues{
 	my %ref = %{$_[0]};
+	my @ref_arr = @{$ref{val}};
+	push(@ref_arr, $_[1]);
+	#print Dumper \@ref_arr;
 	$ref{sum} += $_[1];
 	$ref{cnt}++;
+	$ref{val} = \@ref_arr;
 	$_[0] = \%ref;
+}
+sub doStats{
+	my $math_ref = Math::NumberCruncher->new();
+	my %ref = %{$_[0]};
+	my @ref_arr = @{$ref{val}};
+	$ref{mean} = $math_ref->Mean(\@ref_arr);		#mean
+	$ref{medi} = $math_ref->Median(\@ref_arr);		#median
+	
+	my $std = $math_ref->StandardDeviation(\@ref_arr); #stdDev
+	my $stdVal;
+	my $temp = new IO::Scalar \$stdVal;
+	$temp->print($std);				#printing to var to lose the bless
+	$ref{stdDev} = $stdVal;
+	
+	$_[0] = \%ref;
+}
+sub compPrint{
+	my %ref = %{$_[0]};
+	my $presPos = $_[1];
+	my @dat = @{$_[2]};
+	
+	print $presPos. "- ". $dat[0] . " " . $dat[1] ."\n";
+	
 }
 
 @ARGV or die "Usage: $0 team week\n";
 
 my $teamNum = $ARGV[0];
 my $weekNum = $ARGV[1];
+
 
 #calc num of teams
 my $dir = dir("week" . $weekNum."JSON");
@@ -27,29 +58,31 @@ my $nfiles = $dir->traverse(sub {
 $nfiles-=1;	#offset for hash file;
 
 #for stats
-my $QB_ref = { sum => 0, cnt => 0};
-my $RB_ref = { sum => 0, cnt => 0};
-my $WR_ref = { sum => 0, cnt => 0};
-my $TE_ref = { sum => 0, cnt => 0};
-my $K_ref = { sum => 0, cnt => 0};
-my $DEF_ref = { sum => 0, cnt => 0};
-
-
+my $QB_ref = { sum => 0, cnt => 0, val => []};
+my $RB_ref = { sum => 0, cnt => 0, val => []};
+my $WR_ref = { sum => 0, cnt => 0, val => []};
+my $TE_ref = { sum => 0, cnt => 0, val => []};
+my $K_ref = { sum => 0, cnt => 0, val => []};
+my $DEF_ref = { sum => 0, cnt => 0, val => []};
 
 
 #print "LINE 29\n";							#DEBUG
-
+my %myTeam;
 my @teams = ();						#array of hash references
 my $json = JSON->new->allow_nonref;
 my $file1 = $dir->file("hash.txt");
 my $str1 = $file1->slurp();
 my @teamNames = @{$json->decode( $str1 )};
-for(my $i=0;$i<$nfiles;$i++){
+my $herp = scalar @teamNames;
+for(my $i=0;$i<$herp;$i++){
 	#print "Getting numbers from: " . $teamNames[$i] . " ($i)\n"; #DEBUG
 	my $file = $dir->file("team".($i+1).".txt");
 	my $str = $file->slurp();
 	$teams[$i] = $json->decode( $str );
 	my %team = %{$teams[$i]};
+	if($i==$teamNum){
+		%myTeam = %team;
+	}
 	for my $key (keys %team) {
    	 	my @data = @{$team{$key}};
    	 	my @info = split / /, $data[1];
@@ -65,6 +98,13 @@ for(my $i=0;$i<$nfiles;$i++){
    	 	}
 	}
 }
+doStats($QB_ref);
+doStats($RB_ref);
+doStats($WR_ref);
+doStats($TE_ref);
+doStats($K_ref);
+doStats($DEF_ref);
+
 my %QB = %{$QB_ref};
 my %RB = %{$RB_ref};
 my %WR = %{$WR_ref};
@@ -73,9 +113,56 @@ my %K = %{$K_ref};
 my %DEF = %{$DEF_ref};
 print "\n\tStats\n";
 print "+-------------------------------------------------------------+\n";
-print "QB-\tSum: ". $QB{sum} ."\tCnt: " . $QB{cnt} ."\tAvg: ".($QB{sum}/$QB{cnt})."\n";
-print "RB-\tSum: ". $RB{sum} ."\tCnt: " . $RB{cnt} ."\tAvg: ".($RB{sum}/$RB{cnt})."\n";
-print "WR-\tSum: ". $WR{sum} ."\tCnt: " . $WR{cnt} ."\tAvg: ".($WR{sum}/$WR{cnt})."\n";
-print "TE-\tSum: ". $TE{sum} ."\tCnt: " . $TE{cnt} ."\tAvg: ".($TE{sum}/$TE{cnt})."\n";
-print "KR-\tSum: ". $K{sum} ."\tCnt: " . $K{cnt} ."\tAvg: ".($K{sum}/$K{cnt})."\n";
-print "DF-\tSum: ". $DEF{sum} ."\tCnt: " . $DEF{cnt} ."\tAvg: ".($DEF{sum}/$DEF{cnt})."\n\n";
+print "QB- Sum: ". $QB{sum} ."  Cnt: " . $QB{cnt} ."  Avg: ".($QB{mean})."  Std Dev: ".($QB{stdDev})."\n";
+print "RB- Sum: ". $RB{sum} ."  Cnt: " . $RB{cnt} ."  Avg: ".($RB{mean})."  Std Dev: ".($RB{stdDev})."\n";
+print "WR- Sum: ". $WR{sum} ."  Cnt: " . $WR{cnt} ."  Avg: ".($WR{mean})."  Std Dev: ".($WR{stdDev})."\n";
+print "TE- Sum: ". $TE{sum} ."  Cnt: " . $TE{cnt} ."  Avg: ".($TE{mean})."  Std Dev: ".($TE{stdDev})."\n";
+print "KR- Sum: ". $K{sum} ."  Cnt: " . $K{cnt} ."  Avg: ".($K{mean})."  Std Dev: ".($K{stdDev})."\n";
+print "DF- Sum: ". $DEF{sum} ."  Cnt: " . $DEF{cnt} ."  Avg: ".($DEF{mean})."  Std Dev: ".($DEF{stdDev})."\n\n";
+
+for my $key (keys %myTeam) {
+	my @data = @{$myTeam{$key}};
+	my @info = split / /, $data[1];
+	my $pos = $info[-1];
+	switch($pos){
+		case "QB"	{compPrint($QB_ref, $key, \@data)}
+		case "RB"	{compPrint($RB_ref, $key, \@data)}
+		case "WR"   {compPrint($WR_ref, $key, \@data)}
+		case "TE"	{compPrint($TE_ref, $key, \@data)}
+		case "K"	{compPrint($K_ref, $key, \@data)}
+		case "DEF"	{compPrint($DEF_ref, $key, \@data)}
+		else		{print $pos . " is invalid!\n"}
+	}
+}
+
+my $dir = dir("week" . $weekNum."JSON", "stats");
+$dir->mkpath();
+my $json_text1   = $json->encode($QB_ref);
+my $file = $dir->file("QB.txt");
+my $file_handle = $file->openw();
+$file_handle->print($json_text1);
+
+$json_text1   = $json->encode($RB_ref);
+$file = $dir->file("RB.txt");
+$file_handle = $file->openw();
+$file_handle->print($json_text1);
+
+$json_text1   = $json->encode($WR_ref);
+$file = $dir->file("WR.txt");
+$file_handle = $file->openw();
+$file_handle->print($json_text1);
+
+$json_text1   = $json->encode($TE_ref);
+$file = $dir->file("TE.txt");
+$file_handle = $file->openw();
+$file_handle->print($json_text1);
+
+$json_text1   = $json->encode($K_ref);
+$file = $dir->file("K.txt");
+$file_handle = $file->openw();
+$file_handle->print($json_text1);
+
+$json_text1   = $json->encode($DEF_ref);
+$file = $dir->file("DEF.txt");
+$file_handle = $file->openw();
+$file_handle->print($json_text1);
---
+++ ffiend.pl
@@ -0,0 +1,430 @@
+use IO::Prompt;
+use WWW::Mechanize;
+use strict;
+use Data::Dumper;
+use HTML::TableExtract;
+use Path::Class;
+use Path::Class::Dir;
+use List::Util qw(sum);
+use JSON;
+use IO::Scalar;
+use Switch;
+use Math::Complex;
+use Math::NumberCruncher;
+use Text::Levenshtein qw(distance);
+#Globals
+my %myTeam = {};		#hash of team members for selected team see "analyzer"
+my @teams = ();		#arr of team hashes  see "analyzer"
+my @teamNames = ();	#arr of team names
+
+
+sub help(){
+	print "Usage:\n";
+	print "'scrape'  --- scrapes team and players\n";
+	print "'analyze' --- analyzes data\n";
+	print "'run'  --- does scrape then analyze\n";
+	print "'comp first1 last1 first2 last2' --- displays stats side by side and compare stats\n";
+}
+sub checkWeek{
+	my $mech = WWW::Mechanize->new();
+	$mech->get("http://www.nfl.com/scores");
+	my $weekCon = $mech->content;
+	my $week = 0;
+	$weekCon =~ m!.*<h2.*>(.*)</h2>!;
+	my @temp1 = split / /, $1;
+	my $weekVer = prompt("Is this for Week #" . $temp1[1]. "?(y/n) ", -yn);
+	if(!$weekVer){
+		$week = prompt("Please enter the week number: ", -raw);
+	}else{
+		$week = $temp1[1];
+	}
+	return $week;
+}
+sub getCount{
+	$_[0] =~ m!<select id="teamId" name="teamId">(.*)</select>!;
+	my @temp1 = split /<.*?>/, $1;
+	return @temp1;
+}
+sub addValues{
+	my %ref = %{$_[0]};
+	my @ref_arr = @{$ref{val}};
+	push(@ref_arr, $_[1]);
+	#print Dumper \@ref_arr;
+	$ref{sum} += $_[1];
+	$ref{cnt}++;
+	$ref{val} = \@ref_arr;
+	$_[0] = \%ref;
+}
+sub doStats{
+	my $math_ref = Math::NumberCruncher->new();
+	my %ref = %{$_[0]};
+	my @ref_arr = @{$ref{val}};
+	$ref{mean} = $math_ref->Mean(\@ref_arr);		#mean
+	$ref{medi} = $math_ref->Median(\@ref_arr);		#median
+	
+	my $std = $math_ref->StandardDeviation(\@ref_arr); #stdDev
+	my $stdVal;
+	my $temp = new IO::Scalar \$stdVal;
+	$temp->print($std);				#printing to var to lose the bless
+	$ref{stdDev} = $stdVal;
+	
+	$_[0] = \%ref;
+}
+sub compPrint{
+	my %ref = %{$_[0]};
+	my $presPos = $_[1];
+	my @dat = @{$_[2]};
+	
+	print $presPos. "- ". $dat[0] . " " . $dat[1] ."\n";
+	
+}
+sub scraper {
+	my $usrn = $_[0];
+	my $pswd = $_[1];
+	my $week = $_[2];
+	my $url = "http://fantasy.nfl.com/league/" . $_[3];
+	
+	my $mech = WWW::Mechanize->new();
+	$mech->get($url);
+	$mech->submit_form(
+			form_number => 1,
+			fields      => {
+				username    => $usrn,
+				password    => $pswd,
+			}
+	);
+	my $tUrl = $url . "/team/";
+	$mech->get($tUrl . "1");
+	my @tmpNames = getCount($mech->content);
+	@teamNames = ();
+	foreach my $tm(@tmpNames){
+		#print "Comparing " . $tm ."\n";
+		if(length($tm)>0){
+			#print "Pushing " . $tm . "\n";
+			push(@teamNames, $tm);
+		}
+	}
+	my $numTeams = (scalar @teamNames);
+	my $dir1 = "week".$week;
+	mkdir $dir1;
+	my $dir = dir($dir1);
+	print "Parsing Teams:\n";
+	for(my $j = 1; $j<=$numTeams; $j++){
+		print $teamNames[$j-1]."($j)";
+		$mech->get($tUrl . $j);
+		print ".";
+		my $te = HTML::TableExtract->new();
+		my $html = $mech->content;
+		$te->parse($html);
+		print ".";
+		my %disTeam;
+		# Examine all matching tables
+		foreach my $ts ($te->tables) {
+			print ".";
+			#print "Table (", join(',', $ts->coords), "): ";
+			my @rows = $ts->rows();
+			my $lim = scalar @rows;
+			#print $lim . " rows long\n";
+			if($ts->count==0){
+				if($j==5){
+					my @info = split / -/, $ts->cell(2,3);
+					$disTeam{"QB"} = [$ts->cell(2,-1), $info[0]];
+					@info = split / -/, $ts->cell(3,3);
+					$disTeam{"RB1"} = [$ts->cell(3,-1), $info[0]];
+					@info = split / -/, $ts->cell(4,3);
+					$disTeam{"RB2"} = [$ts->cell(4,-1), $info[0]];
+					@info = split / -/, $ts->cell(5,3);
+					$disTeam{"WR1"} = [$ts->cell(5,-1), $info[0]];
+					@info = split / -/, $ts->cell(6,3);
+					$disTeam{"WR2"} = [$ts->cell(6,-1), $info[0]];
+					@info = split / -/, $ts->cell(7,3);
+					$disTeam{"TE"} = [$ts->cell(7,-1), $info[0]];
+					@info = split / -/, $ts->cell(8,3);
+					$disTeam{"W/R"} = [$ts->cell(8,-1), $info[0]];
+					#Skip 9 for "Bench" row
+					for(my $i=10; $i<$lim; $i++){
+						@info = split / -/, $ts->cell($i,3);
+						$disTeam{"BN".($i-9)} = [$ts->cell($i,-1), $info[0]];
+					
+					}
+					
+				}else{
+					my @info = split / -/, $ts->cell(2,2);
+					$disTeam{"QB"} = [$ts->cell(2,-1), $info[0]];
+					@info = split / -/, $ts->cell(3,2);
+					$disTeam{"RB1"} = [$ts->cell(3,-1), $info[0]];
+					@info = split / -/, $ts->cell(4,2);
+					$disTeam{"RB2"} = [$ts->cell(4,-1), $info[0]];
+					@info = split / -/, $ts->cell(5,2);
+					$disTeam{"WR1"} = [$ts->cell(5,-1), $info[0]];
+					@info = split / -/, $ts->cell(6,2);
+					$disTeam{"WR2"} = [$ts->cell(6,-1), $info[0]];
+					@info = split / -/, $ts->cell(7,2);
+					$disTeam{"TE"} = [$ts->cell(7,-1), $info[0]];
+					@info = split / -/, $ts->cell(8,2);
+					$disTeam{"W/R"} = [$ts->cell(8,-1), $info[0]];
+					#Skip 9 for "Bench" row
+					for(my $i=10; $i<$lim; $i++){
+						@info = split / -/, $ts->cell($i,2);
+						$disTeam{"BN".($i-9)} = [$ts->cell($i,-1), $info[0]];
+					
+					}
+				}
+			}
+			if($ts->count==1){
+				if($j==5){
+					my @info = split / -/, $ts->cell(2,3);
+					$disTeam{"K"} = [$ts->cell(2,-1), $info[0]];
+				}else{
+					my @info = split / -/, $ts->cell(2,2);
+					$disTeam{"K"} = [$ts->cell(2,-1), $info[0]];	
+				}
+			}
+			if($ts->count==2){
+				if($j==5){
+					my @info = split /-/, $ts->cell(2,3);
+					$disTeam{"DEF"} = [$ts->cell(2,-1), $info[0]];
+				}else{
+					my @info = split /-/, $ts->cell(2,2);
+					$disTeam{"DEF"} = [$ts->cell(2,-1), $info[0]];	
+				}
+			}
+		}
+		#$fTeams[$j-1] = {%disTeam};
+		my $json = JSON->new->allow_nonref;
+		my $json_text   = $json->encode(\%disTeam);
+		my $file = $dir->file("team". $j .".txt");
+		my $file_handle = $file->openw();
+		$file_handle->print($json_text);
+		#print $json_text . "\n";
+		print "done!\n";
+		#print Dumper \%disTeam;
+		
+	}
+	my $json = JSON->new->allow_nonref;
+	my $json_text   = $json->encode(\@teamNames);
+	my $file = $dir->file("hash.txt");
+	my $file_handle = $file->openw();
+	$file_handle->print($json_text);
+}
+sub scrape{
+	my $week = checkWeek();
+	my $league = prompt("Please enter your league number: ", -raw);
+	my $usrn = prompt("Username: ", -raw);
+	my $pswd = prompt("Password: ", -rawe => '*');
+	scraper($usrn,$pswd,$week,$league);
+}
+sub analyzer{
+	my $teamNum = $_[0];
+	my $weekNum = $_[1];
+	#calc num of teams
+	my $dir = dir("week" . $weekNum);
+	
+	#for stats
+	my $QB_ref = { sum => 0, cnt => 0, val => []};
+	my $RB_ref = { sum => 0, cnt => 0, val => []};
+	my $WR_ref = { sum => 0, cnt => 0, val => []};
+	my $TE_ref = { sum => 0, cnt => 0, val => []};
+	my $K_ref = { sum => 0, cnt => 0, val => []};
+	my $DEF_ref = { sum => 0, cnt => 0, val => []};
+	
+	%myTeam = {};
+	
+	#print "LINE 29\n";							#DEBUG
+	@teams = ();						#array of hash references
+	my $json = JSON->new->allow_nonref;
+	my $file1 = $dir->file("hash.txt");
+	my $str1 = $file1->slurp();
+	@teamNames = @{$json->decode( $str1 )};
+	my $herp = scalar @teamNames;
+	if($herp == 0){
+		print "Can not access team files, have you run Scraper?\n";
+		return;
+	}
+	for(my $i=0;$i<$herp;$i++){
+		#print "Getting numbers from: " . $teamNames[$i] . " ($i)\n"; #DEBUG
+		my $file = $dir->file("team".($i+1).".txt");
+		my $str = $file->slurp();
+		$teams[$i] = $json->decode( $str );
+		my %team = %{$teams[$i]};
+		if($i==$teamNum){
+			%myTeam = %team;
+		}
+		for my $key (keys %team) {
+			my @data = @{$team{$key}};
+			my @info = split / /, $data[1];
+			my $pos = $info[-1];
+			switch($pos){
+				case "QB"	{addValues($QB_ref,$data[0])}
+				case "RB"	{addValues($RB_ref,$data[0])}
+				case "WR"   {addValues($WR_ref,$data[0])}
+				case "TE"	{addValues($TE_ref,$data[0])}
+				case "K"	{addValues($K_ref,$data[0])}
+				case "DEF"	{addValues($DEF_ref,$data[0])}
+				else		{print $pos . " is invalid!\n"}
+			}
+		}
+	}
+	doStats($QB_ref);
+	doStats($RB_ref);
+	doStats($WR_ref);
+	doStats($TE_ref);
+	doStats($K_ref);
+	doStats($DEF_ref);
+	
+	my %QB = %{$QB_ref};
+	my %RB = %{$RB_ref};
+	my %WR = %{$WR_ref};
+	my %TE = %{$TE_ref};
+	my %K = %{$K_ref};
+	my %DEF = %{$DEF_ref};
+	print "\n\tStats\n";
+	print "+-------------------------------------------------------------+\n";
+	print "QB- Sum: ". $QB{sum} ."  Cnt: " . $QB{cnt} ."  Avg: ".($QB{mean})."  Std Dev: ".($QB{stdDev})."\n";
+	print "RB- Sum: ". $RB{sum} ."  Cnt: " . $RB{cnt} ."  Avg: ".($RB{mean})."  Std Dev: ".($RB{stdDev})."\n";
+	print "WR- Sum: ". $WR{sum} ."  Cnt: " . $WR{cnt} ."  Avg: ".($WR{mean})."  Std Dev: ".($WR{stdDev})."\n";
+	print "TE- Sum: ". $TE{sum} ."  Cnt: " . $TE{cnt} ."  Avg: ".($TE{mean})."  Std Dev: ".($TE{stdDev})."\n";
+	print "KR- Sum: ". $K{sum} ."  Cnt: " . $K{cnt} ."  Avg: ".($K{mean})."  Std Dev: ".($K{stdDev})."\n";
+	print "DF- Sum: ". $DEF{sum} ."  Cnt: " . $DEF{cnt} ."  Avg: ".($DEF{mean})."  Std Dev: ".($DEF{stdDev})."\n\n";
+	
+	for my $key (keys %myTeam) {
+		my @data = @{$myTeam{$key}};
+		my @info = split / /, $data[1];
+		my $pos = $info[-1];
+		switch($pos){
+			case "QB"	{compPrint($QB_ref, $key, \@data)}
+			case "RB"	{compPrint($RB_ref, $key, \@data)}
+			case "WR"   {compPrint($WR_ref, $key, \@data)}
+			case "TE"	{compPrint($TE_ref, $key, \@data)}
+			case "K"	{compPrint($K_ref, $key, \@data)}
+			case "DEF"	{compPrint($DEF_ref, $key, \@data)}
+			else		{print $pos . " is invalid!\n"}
+		}
+	}
+	
+	my $dir = dir("week" . $weekNum, "stats");
+	$dir->mkpath();
+	my $json_text1   = $json->encode($QB_ref);
+	my $file = $dir->file("QB.txt");
+	my $file_handle = $file->openw();
+	$file_handle->print($json_text1);
+	
+	$json_text1   = $json->encode($RB_ref);
+	$file = $dir->file("RB.txt");
+	$file_handle = $file->openw();
+	$file_handle->print($json_text1);
+	
+	$json_text1   = $json->encode($WR_ref);
+	$file = $dir->file("WR.txt");
+	$file_handle = $file->openw();
+	$file_handle->print($json_text1);
+	
+	$json_text1   = $json->encode($TE_ref);
+	$file = $dir->file("TE.txt");
+	$file_handle = $file->openw();
+	$file_handle->print($json_text1);
+	
+	$json_text1   = $json->encode($K_ref);
+	$file = $dir->file("K.txt");
+	$file_handle = $file->openw();
+	$file_handle->print($json_text1);
+	
+	$json_text1   = $json->encode($DEF_ref);
+	$file = $dir->file("DEF.txt");
+	$file_handle = $file->openw();
+	$file_handle->print($json_text1);
+}
+sub analyze{
+	my $team = prompt("Choose team: ",-raw);
+	my $week = checkWeek();
+	analyzer($team,$week);
+}
+sub run{
+	scrape();
+	analyze();
+}
+sub getPlayer{
+	my $p1 = $_[0];
+	my @res1 = ();
+	my $comp1;
+	my $len  = scalar @teams;
+	for(my $i=0;$i<$len ;$i++){
+		my %team = %{$teams[$i]};
+		for my $key (keys %team) {
+			my @data = @{$team{$key}};
+			my @info = split / /, $data[1];
+			my $pos = $info[-1];
+			my $dist1 = distance($p1, $info[0]." ".$info[1]);
+			if($dist1<(length($p1)/2)){
+				#print $info[0]." ".$info[1]." is only $dist1 off of $p1!\n";
+				push(@data, $i);
+				push(@res1, \@data);
+			}
+		}
+	}
+	if(scalar @res1 == 0){
+		print "No results found for $p1.\n";
+	}elsif(scalar @res1 == 1){
+		$comp1 = $res1[0];
+	}else{
+		my $x = 1;
+		foreach(@res1){
+			my @temp = @{$_};
+			print "$x) ".$temp[1]."\n";
+			$x++;
+		}
+		my $choi = prompt "Choose Result: ", -raw, -i;
+		$comp1 = $res1[$choi-1];
+	}
+	return $comp1;
+}
+sub comp{
+	my @p1 = @{getPlayer($_[0])};
+	print $teamNames[$p1[2]]." owns ".$p1[1]." : ".$p1[0]."\n"; 
+	#print "Got ".$p1[1]."\n";
+	my @p2 = @{getPlayer($_[1])};
+	print $teamNames[$p2[2]]." owns ".$p2[1]." : ".$p2[0]."\n"; 
+	#print "Got ".$p2[1]."\n";
+	
+}
+print "\t#########################################\n";
+print "\t#\tFantasyFiend v0.1\t\t#\n";
+print "\t#\tBy: James Watterson\t\t#\n";
+print "\t#########################################\n\n";
+my @choices = qw(scrape analyze);
+while(prompt "ff=>"){
+	my $in = $_;
+	my @spli = split / /, $in;
+	if(length($in)>0){
+		if ($in eq "quit" || $in eq "q"){
+			last;
+		}
+		elsif ($in eq "lyze" || $in eq "analyze"){
+			analyze();
+		}
+		elsif ($in eq "scrape" || $in eq "scrap"){
+			scrape();
+		}
+		elsif ($in eq "h" || $in eq "help" || $in eq "?"){
+			help();
+		}
+		elsif ($in eq "r" || $in eq "run"){
+			run();
+		}
+		elsif ($spli[0] eq "c" || $spli[0] eq "comp" || $spli[0] eq "compare"){
+			#print (scalar @spli)."\n";
+			if((scalar @teams)>0){
+				if((scalar @spli) == 5){
+					comp($spli[1]." ".$spli[2], $spli[3]." ".$spli[4]);
+				}else{
+					print "Invalid Args, do:\ncomp first1 last1 first2 last2\n";
+				}
+			}else{
+				print "Teams array is empty, run analyzer.\n"
+			}
+		}
+		else{
+			print "command: '$in' not recognized\n";
+		}
+	}
+}
